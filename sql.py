4b3b09e317dae5300014a788536b7037dab98cc7
zone <qmopla@naver.com>
2017-09-25 18:53:04 +0900

    완료

--- sql.py
+++ sql.py
@@ -13,6 +13,35 @@ connect.set_character_set('utf8')
 connect.autocommit = True
 c = connect.cursor()
 
+# sql_drop = "drop table if exists %s" % ("wiki_invert")
+# sql = "create table %s (%s varchar(1000) not null, %s int(11) not null)" % ("wiki_invert", "term", "id")
+# c.execute(sql_drop)
+# c.execute(sql)
+
+sql2 = "SELECT * FROM wiki"
+c.execute(sql2)
+wiki = c.fetchall()
+
+wordlist = ['also', 'debut', 'language', 'two']
+idlist = []
+idxdict = {}
+
+for w in wordlist:
+    for line in wiki:
+        txt_li = line[2].split()
+        #print(txt_li)
+        if w in txt_li:
+            if line[0] not in idlist:
+                idlist.append(line[0])
+    idxdict[w] = idlist
+    idlist = []
+
+for w in wordlist:
+    ids = idxdict[w]
+    for id in ids:
+        sql3 = "INSERT INTO wiki_invert VALUES ('%s',%d)"%(w,id)
+        c.execute(sql3)
+        connect.commit()
 
 def tf(id,term):
 
@@ -25,27 +54,23 @@ def tf(id,term):
     for txt in list:
         if txt.lower() == term:
             count += 1
-    print(length)
-    print(count)
+    #print(length)
+    #print(count)
     return math.log(1+(count/length))
 
 def idf(term):
-    sql_drop = "drop table if exists %s"%("wiki_invert")
-    sql = "create table %s (%s varchar(1000) not null, %s int(11) not null)"%("wiki_invert","term","id")
-    c.execute(sql_drop)
+    sql = "SELECT COUNT(DISTINCT id) FROM wiki_invert WHERE term = '{}'".format(term)
     c.execute(sql)
-
-    
-
-        # for i in range(len(id_tf)):
-        #     arrange_txt = list(txt_tf[i])
-        #     #print(arrange_txt)
-        #     string_txt = arrange_txt[0].split(" ")
-        #     for i in string_txt:
+    nt = c.fetchone()[0]
+    nt_int = int(nt)
+    return 1/nt_int
 
 
-# tf(41631770,"also")
-# tf(6688599,"debut")
-# tf(13794826,"language")
+def tf_idf(id,term):
+    ti = tf(id, term) * idf(term)
+    print("('{term}' in {id}) TF-IDF: {ti}".format(term=term, id=id, ti=ti))
 
-idf('k')
+
+tf_idf(41631770, 'also')
+tf_idf(6688599, 'debut')
+tf_idf(13794826, 'language')
